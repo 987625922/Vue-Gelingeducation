2bced415da6d8159a3e56bea7b32db32dbb4ef45
wind <987625922@qq.com>
2020-07-17 18:04:02 +0800

    更新

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "http://122.51.177.223:8889"
-const BASE_URL = "http://localhost:8081"
+const BASE_URL = "http://122.51.177.223:8889"
+// const BASE_URL = "http://localhost:8081"
 export default{
   BASE_URL
 }
